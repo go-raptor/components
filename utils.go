84764c14f0a1b400744db2e03b16d5717272d4be
Krunoslav Husak <kruno@husak.me>
2025-02-26 16:48:27 +0100

    Refactor Raptor components into their own package

---
+++ utils.go
@@ -0,0 +1,58 @@
+package components
+
+import (
+	"log/slog"
+	"os"
+	"strings"
+
+	"github.com/go-raptor/config"
+	"github.com/go-raptor/connector"
+	"github.com/pwntr/tinter"
+)
+
+type Utils struct {
+	Config *config.Config
+
+	Log      *slog.Logger
+	logLevel *slog.LevelVar
+
+	DB connector.DatabaseConnector
+}
+
+func NewUtils() *Utils {
+	levelVar := &slog.LevelVar{}
+
+	opts := &tinter.Options{
+		Level: levelVar,
+	}
+
+	return &Utils{
+		Log:      slog.New(tinter.NewHandler(os.Stderr, opts)),
+		logLevel: levelVar,
+	}
+}
+
+func (u *Utils) SetDB(db connector.DatabaseConnector) {
+	u.DB = db
+}
+
+func (u *Utils) SetConfig(config *config.Config) {
+	u.Config = config
+}
+
+func (u *Utils) SetLogLevel(logLevel string) {
+	var level slog.Level
+	switch strings.ToUpper(logLevel) {
+	case "DEBUG":
+		level = slog.LevelDebug
+	case "INFO":
+		level = slog.LevelInfo
+	case "WARN":
+		level = slog.LevelWarn
+	case "ERROR":
+		level = slog.LevelError
+	default:
+		level = slog.LevelInfo
+	}
+	u.logLevel.Set(level)
+}
